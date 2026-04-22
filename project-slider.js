0ae4dbb744fa3bf8595ee71131ac4d8220578991
aaimar189 <andrea_aimar@yahoo.com>
2026-04-22 14:29:35 +0800

    Update project-slider.js

--- project-slider.js
+++ project-slider.js
@@ -5,26 +5,18 @@ document.addEventListener("DOMContentLoaded", function () {
     const slider = document.getElementById('slider');
 
     function updateSlider() {
-        slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+        slider.style.transform = `translateX(-${currentIndex * 100}vw)`;
     }
 
     window.next = function () {
         currentIndex++;
-
-        if (currentIndex >= slides.length) {
-            currentIndex = 0;
-        }
-
+        if (currentIndex >= slides.length) currentIndex = 0;
         updateSlider();
     }
 
     window.prev = function () {
         currentIndex--;
-
-        if (currentIndex < 0) {
-            currentIndex = slides.length - 1;
-        }
-
+        if (currentIndex < 0) currentIndex = slides.length - 1;
         updateSlider();
     }
 
